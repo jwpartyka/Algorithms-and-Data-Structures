896bfc33be0a4847d70ac371c6b6e9a9c389184d
jonparty <34322460+jonparty@users.noreply.github.com>
2017-12-22 21:12:58 +0100

    Add files via upload

---
+++ range_queries/Fenwick.cpp
@@ -0,0 +1,29 @@
+//Drzewo potęgowe (+, suma)
+
+int Tree[2*1024*1024], Base = 1;
+
+int f(int x)
+{
+    return x & (-x);
+}
+
+void chTree(int node, int v)
+{
+    while (node <= Base)
+    {
+        Tree[node] += v;
+        node += f(node);
+    }
+}
+
+int read(int node)
+{
+    if (node < 0) return 0;
+    int res = 0;
+    while (node)
+    {
+        res += Tree[node];
+        node -= f(node);
+    }
+    return res;
+}
