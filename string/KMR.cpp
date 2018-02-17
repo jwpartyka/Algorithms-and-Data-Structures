6753b1bb8f34c8844e16ad5e1be1b30c10fa48a0
jonparty <34322460+jonparty@users.noreply.github.com>
2018-02-17 15:27:13 +0100

    Update KMR.cpp

--- string/KMR.cpp
+++ string/KMR.cpp
@@ -1,77 +1,103 @@
-#define para pair<pair<int, int>,int>
-#define st first
-#define nd second
+const int MAXN = 5e5 + 5;
+
+struct Pair
+{
+    int h1, h2, pos;
+    bool operator != (const Pair &b) const
+    {
+        return (h1 != b.h1 || h2 != b.h2);
+    }
+};
 
 struct kmr
 {
-    //Tablica podsłów bazowych
+    //Tablica haszy dla posłów bazowych:
     vector<vector<int>> KMR;
-    //pp[i] = wykładnik przy największej potędze dwójki mniejszej od i
+    //Tablica do sortowania kubełkowego:
+    vector<vector<vector<Pair>>> bucket;
+    //pp[i] - wykładnik przy największej potędze dwójki mniejszej od lub równej i
     vector<int> pp;
-    //długość słowa, log2(N), rozmiar alfabetu
-    int N, LOG, SIGMA;
+    int n; // długość słowa
 
-    kmr(string &s) : N(s.size()), LOG(ceil(log2(s.size()))), SIGMA(255), pp(s.size()+3), KMR(ceil(log2(s.size())+1), vector<int>(s.size()+500, 0))
+    kmr(string &s) : n(s.size()), pp(s.size() + 3), bucket(2, vector<vector<Pair>>(max((int)s.size(), 355)))
     {
-        //Hasze dla fragmentów długości 1 to ich kod ASCII
-        for (int i = 0; i < N; i++)
+        int ALPHA = 0; //Wartość największego hasza
+        vector<Pair> pairs(n);
+        //lex[i] - hasz podsłowa zaczynającego się w s[i]
+        vector<int> lex(n);
+
+        //Hasze dla podsłów długości 1 to ich numery ASCII
+        for (int i = 0; i < n; i++)
         {
-            KMR[0][i] = (int)s[i];
+            lex[i] = (int)s[i];
+            ALPHA = max(ALPHA, lex[i]);
         }
 
-        //Obliczanie haszy dla fragmentów długości 2^(j+1) dla j < log2(N)
-        for (int j = 0; j < LOG; j++)
+        KMR.push_back(lex);
+
+        //Obliczanie haszy dla podsłów długości 2 ^ (p + 1):
+        int p = 2;
+        while (p < n)
         {
-            static vector<para> bucket[2][MAXN];
-            for (int i = 0; i < N; i++)
+            lex.resize(n - p + 1);
+            pairs.resize(n - p + 1);
+            for (int i = 0; i <= n - p; i++)
             {
-                //Początkowo drugi element pary to 0, bo może on być z poza słowa
-                para suf = {{KMR[j][i], 0}, i};
-                //Jeżeli drugi element pary mieści się w słowie:
-                if (i + (1 << j) < N)
-                {
-                    suf.st.nd = KMR[j][i + (1 << j)];
-                }
-                //Sortuj kubełkowo po drugim elemencie pary
-                bucket[0][suf.st.nd].push_back(suf);
+                pairs[i] = {lex[i], lex[i + p / 2], i};
             }
 
-            for (int k = 0; k <= SIGMA; k++)
-            {
-                for (auto i : bucket[0][k])
-                {
-                    //Sortuj kubełkowo po pierwszym elemencie pary
-                    bucket[1][i.st.st].push_back(i);
-                }
-                bucket[0][k].clear();
-                bucket[0][k].shrink_to_fit();
-            }
+            //Sortowanie kubełkowe wszystkich par:
+            bucket_sort(pairs, ALPHA);
 
-            int hasz = 0;
-            para prev = {{0, 0}, 0};
+            ALPHA = 0;
 
-            for (int k = 0; k <= SIGMA; k++)
+            //Zahaszowanie podsłów:
+            lex[pairs[0].pos] = 0;
+            for (int i = 1; i <= n - p; i++)
             {
-                for (auto i : bucket[1][k])
-                {
-                    //Jeżeli obecna para i poprzednia są takie same, to mają takie same hasze
-                    //W przeciwnym wypadku hasz obecnej pary jest większy o jeden
-                    KMR[j + 1][i.nd] = hasz + (prev.st != i.st);
-                    hasz = KMR[j + 1][i.nd];
-                    prev = i;
-                }
-                bucket[1][k].clear();
-                bucket[1][k].shrink_to_fit();
+                //Jeżeli dwa podsłówa są takie same to mają taki sam hasz
+                if (pairs[i] != pairs[i - 1]) ALPHA++;
+                lex[pairs[i].pos] = ALPHA;
             }
-            SIGMA = hasz;
+            KMR.push_back(lex);
+            p *= 2;
         }
 
-        for (int i = 1; i < LOG; i++)
+        p = 0;
+        for (int i = 1; i <= n; i++)
         {
-            for (int j = (1 << i) + 1; j <= (1 << (i + 1)) && j <= N; j++)
+            if (__builtin_popcount(i) == 1) p++;
+            pp[i] = p;
+        }
+    }
+
+    void bucket_sort(vector<Pair> &pairs, int ALPHA)
+    {
+        //Sortowanie po drugim elemencie pary
+        for (auto p : pairs)
+        {
+            bucket[0][p.h2].push_back(p);
+        }
+
+        //Sortowanie po pierwszym elemencie pary
+        for (int i = 0; i <= ALPHA; i++)
+        {
+            for (auto p : bucket[0][i])
             {
-                pp[j] = i;
+                bucket[1][p.h1].push_back(p);
             }
+            bucket[0][i].clear();
+        }
+
+        //Uzupełnianie wektora parami w posortowanej kolejności
+        int ind = 0;
+        for (int i = 0; i <= ALPHA; i++)
+        {
+            for (auto p : bucket[1][i])
+            {
+                pairs[ind++] = p;
+            }
+            bucket[1][i].clear();
         }
     }
 
