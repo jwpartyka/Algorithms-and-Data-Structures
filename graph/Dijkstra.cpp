b51a296e1b91da2513f7ee0dee6fd30e6c09907c
jonparty <34322460+jonparty@users.noreply.github.com>
2018-03-14 20:14:42 +0100

    Update Dijkstra.cpp

--- graph/Dijkstra.cpp
+++ graph/Dijkstra.cpp
@@ -1,30 +1,34 @@
 //Oblicza długości najkrótszych ścieżek od wierzchołka startowego do każdego innego
 //Nie działa gdy jest ujemny cykl w grafie
-//Złożoność O(|E| * log|V|)
+//Złożoność: O(|E| * log|V|)
 
 #define st first
 #define nd second
 
-const int MAXN = 5e5+5, INF = 1e9;
+const int MAXN = 5e5+5, inf = 1e9;
+
 int dist[MAXN]; //Odległości
 vector<pair<int, int>> G[MAXN]; //Graf
 priority_queue<pair<int, int> > Q; //Kolejka do dijkstry, trzyma {-dist[v], v}
 
-void dijkstra(int start)
+void dijkstra(int src, int n)
 {
-    for (int i = 1; i < MAXN; i++) dist[i] = INF;
-    dist[start] = 0;
-    Q.push({0, start});
+    for (int i = 1; i <= n; i++) dist[i] = inf;
+    
+    dist[src] = 0;
+    Q.push({0, src});
+    
     while (!Q.empty())
     {
         int v = Q.top().nd;
-        int ww = Q.top().st;
+        int ww = -Q.top().st;
         Q.pop();
         //Jeżeli dist[v] jest mniejsze niż w czasie wrzucenia v na kolejkę to nie rozważaj wierzchołka v
         if (ww > dist[v]) continue;
-        for (auto i : G[v])
+        
+        for (auto e : G[v])
         {
-            int u = i.st, w = i.nd;
+            int u = e.st, w = e.nd;
             if (dist[u] > dist[v] + w)
             {
                 dist[u] = dist[v] + w;
