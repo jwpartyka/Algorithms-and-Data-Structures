4bbc46832c4d2b615a5acbdefe0e10520f98ae25
jonparty <34322460+jonparty@users.noreply.github.com>
2018-03-20 21:14:31 +0100

    Update Centroid_Decomposition.cpp

--- graph/Centroid_Decomposition.cpp
+++ graph/Centroid_Decomposition.cpp
@@ -1,11 +1,25 @@
 //Znajduje dekompozycje drzewa na centroidy
 //Wyznacza stopnie centroidów i centroidowych rodziców
-//Złożoność O((|V| + |E|) * log|V|)
+//Złożoność: O((|V| + |E|) * log|V|)
 
 const int MAXN = 5e5+5;
+
 int sz[MAXN], cd[MAXN], cdp[MAXN]; //Rozmiary poddrzew, stopnie centroidów, centroidowi rodzice
 vector<int> G[MAXN]; //Graf
 
+//Oblicza rozmiary poddrzew:
+void calcSizes(int v, int p)
+{
+    sz[v] = 1;
+    for (int u : G[v])
+    {
+        if (cd[u]) continue;
+        calcSizes(u, v);
+        sz[v] += sz[u];
+    }
+}
+
+//Znajduje centroid:
 int cdFind(int v, int p)
 {
     int tot = sz[v];
@@ -22,27 +36,34 @@ int cdFind(int v, int p)
     }
     return v;
 }
+
+//Przetwarza centroid:
 void cdCalc(int v, int p)
 {
     cdp[v] = p;
     cd[v] = cd[cdp[v]] + 1;
     /*...*/
 }
+
+//Buduje dekompozycje:
 void cdBuild(int v, int p)
 {
     calcSizes(v, -1); //Oblicza rozmiary poddrzew
     v = cdFind(v, -1); //Znajduje centroid
-    cdCalc(v, p); //Przerabia centroid
+    cdCalc(v, p); //Przetwarza centroid
+    //Wywołuje się dla poddrzew:
     for (int u : G[v])
     {
         if (cd[u]) continue;
         cdBuild(u, v);
     }
 }
-void ansQuery(int v)
+
+void query(int v)
 {
     /* ... */
 }
+
 void update(int v)
 {
     /* ... */
