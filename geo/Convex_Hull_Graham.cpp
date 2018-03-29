6450f4305df8c2096b7578379ca52482845638a5
jonparty <34322460+jonparty@users.noreply.github.com>
2018-03-29 19:16:28 +0200

    Update Convex_Hull_Graham.cpp

--- geo/Convex_Hull_Graham.cpp
+++ geo/Convex_Hull_Graham.cpp
@@ -1,18 +1,29 @@
 //Znajduje otoczkę wypukłą algorytmem Grahama.
 //Żadne 3 wierzchołki otoczki nie są współliniowe.
-//Wierzchołki otoczki są posortowane zgodnie z ruchem wskazówek zegara.
+//Wierzchołki otoczki są ułożone przeciwnie do ruchu wskazówek zegara.
 //Złożoność: O(N * log N)
 //Source: USACO
 
 using ll = long long;
-using point = complex<ll>;
 
-#define X real()
-#define Y imag()
+struct point
+{
+    ll x, y;
+
+    inline point operator - (const point &b) const
+    {
+        return {x - b.x, y - b.y};
+    }
+
+    inline ll operator * (const point &b) const
+    {
+        return (x * b.y) - (y * b.x);
+    }
+};
 
 ll cross(point a, point b)
 {
-    return (conj(a) * b).Y;
+    return a * b;
 }
 
 ll cross(point a, point b, point c)
@@ -20,30 +31,18 @@ ll cross(point a, point b, point c)
     return cross(b - a, c - a);
 }
 
-//Zwraca otoczkę
-vector<point> getHull(vector<point> P)
+ll norm(point a)
 {
-    //Znajduje punkt o minimalnym x, y
-    swap(P[0], *min_element(P.begin(), P.end(), [](point a, point b)
-    {
-        if (a.X == b.X) return a.Y < b.Y;
-        return a.X < b.X;
-    }));
-    //Sortuje kątowo
-    sort(P.begin() + 1, P.end(), [&P](point a, point b)
-    {
-        ll cr = cross(P[0], a, b);
-        if (cr != 0) return cr < 0;
-        return abs(a - P[0]) < abs(b - P[0]);
-    });
+    return a.x * a.x + a.y * a.y;
+}
 
-    //Znajduje otoczkę:
-    vector<point> H;
-    H.push_back(P[0]);
+vector<point> getHull(vector<point> &P)
+{
+    vector<point> H = {P[0]};
     int sz = 1;
     for (int i = 1; i < P.size(); i++)
     {
-        while (sz >= 2 && cross(H[sz - 2], H[sz - 1], P[i]) >= 0)
+        while (sz >= 2 && cross(H[sz - 2], H.back(), P[i]) <= 0)
         {
             sz--;
             H.pop_back();
@@ -51,5 +50,28 @@ vector<point> getHull(vector<point> P)
         sz++;
         H.push_back(P[i]);
     }
+    return H;
+}
+
+vector<point> Graham(vector<point> P)
+{
+    swap(P[0], *min_element(P.begin(), P.end(), [](point a, point b)
+    {
+        if (a.x == b.x) return a.y < b.y;
+        return a.x < b.x;
+    }));
+    sort(P.begin() + 1, P.end(), [&P](point a, point b)
+    {
+        ll cr = cross(P[0], a, b);
+        if (cr == 0) return norm(a - P[0]) < norm(b - P[0]);
+        return cr > 0;
+    });
+    P.erase(unique(P.begin(), P.end(), [](point a, point b)
+    {
+        return (a.x == b.x and a.y == b.y);
+    }), P.end());
+
+    auto H = getHull(P);
+
     return H;
 }
