7ad32003828518c669a1c5381c098ffa16311241
jon <janusz.partyka00@gmail.com>
2018-03-31 21:27:53 +0200

    Update

--- num_theory/Euclid.cpp
+++
@@ -1,5 +0,0 @@
-int NWD(int a, int b)
-{
-    if (a == 0) return b;
-    return NWD(b%a, a);
-}
--- num_theory/Ext_Euclid.cpp
+++
@@ -1,8 +0,0 @@
-//Dla pary (a, b) zwraca taką parę (x, y), że a*x + b*y = NWD(a, b)
-
-pair<int, int> ExtGCD(int a, int b)
-{
-    if (a == 0) return {0, 1};
-    auto p = ExtGCD(b%a, a);
-    return {p.nd - p.st * (b/a), p.st};
-}
