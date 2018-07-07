20da39a77fa2ffbbf162319760793b7e0d763abd
jon <janusz.partyka00@gmail.com>
2018-07-07 13:22:02 +0200

    Update Segment Tree Beats

--- Range Queries/Segment Trees/Segment Tree Beats.cpp
+++ Range Queries/Segment Trees/Segment Tree Beats.cpp
@@ -6,101 +6,79 @@
 
 using ll = long long;
 
-const int MAXN = 3e5 + 5, inf = 1e9 + 7;
+const int MAXN = 3e5 + 5, inf = 1e9;
 
 struct Node
 {
     int max1, max2, cnt, lazy;
     ll sum;
-    Node operator + (const Node &B) const
-    {
-        Node N;
-        if (max1 >= B.max1) N = *this;
-        else N = B;
-        N.sum = sum + B.sum;
-        if (max1 == B.max1)
-        {
-            N.cnt += B.cnt;
-            N.max2 = max(max2, B.max2);
-        }
-        else if (max1 > B.max1) N.max2 = max(max2, B.max1);
-        else N.max2 = max(max1, B.max2);
-        return N;
-    }
 
-    inline void init(int v)
+    Node operator + (Node b)
     {
-        max1 = sum = lazy = v;
-        max2 = 0;
-        cnt = 1;
+        auto a = *this;
+        if (a.max1 == b.max1) return {a.max1, max(a.max2, b.max2), a.cnt + b.cnt, inf, a.sum + b.sum};
+        if (a.max1 < b.max1) swap(a, b);
+        return {a.max1, max(a.max2, b.max1), a.cnt, inf, a.sum + b.sum};
     }
 };
 
 int tab[MAXN], Base = 1;
-Node Tree[2*512*1024];
+Node Tree[1024 * 1024];
 
-inline void update(int node)
+inline void init(int i, int v)
 {
-    Tree[node] = Tree[2*node] + Tree[2*node+1];
+    Tree[i + Base] = {v, -inf, 1, inf, v};
+}
+
+inline void update(int node, int v)
+{
+    if (v >= Tree[node].max1) return;
+    Tree[node].lazy = min(Tree[node].lazy, v);
+    Tree[node].sum -= (ll)(Tree[node].max1 - v) * Tree[node].cnt;
+    Tree[node].max1 =  v;
+}
+
+inline void propagate(int node)
+{
+    update(2 * node, Tree[node].lazy);
+    update(2 * node + 1, Tree[node].lazy);
+    Tree[node].lazy = inf;
 }
 
 void build(int node)
 {
     if (node >= Base) return;
-    build(2*node), build(2*node+1);
-    update(node);
+    build(2 * node);
+    build(2 * node + 1);
+    Tree[node] = Tree[2 * node] + Tree[2 * node + 1];
 }
 
-void minuj(int node, int v)
+void chTree(int node, int lo, int hi, int l, int r, int v)
 {
-    if (v >= Tree[node].max1) return;
-    Tree[node].lazy = min(Tree[node].lazy, v);
-    if (v >= Tree[node].max2)
+    if (r < lo or hi < l) return;
+    if  (v >= Tree[node].max1) return;
+    if (l <= lo and hi <= r and v > Tree[node].max2)
     {
-        Tree[node].sum -= (ll)(Tree[node].max1 - v) * Tree[node].cnt;
-        Tree[node].max1 = v;
-    }
-    if (v <= Tree[node].max2)
-    {
-        minuj(2 * node, v);
-        minuj(2 * node + 1, v);
-        update(node);
-    }
-}
-
-inline void propaguj(int node)
-{
-    for (int i = 2*node; i <= 2*node+1; i++)
-    {
-        Tree[i].lazy = min(Tree[i].lazy, Tree[node].lazy);
-        minuj(i, Tree[i].lazy);
-    }
-}
-
-void chTree(int node, int left, int right, int l, int r, int v)
-{
-    if (right < l || r < left) return;
-    if (l <= left && right <= r)
-    {
-        minuj(node, v);
+        update(node, v);
         return;
     }
-    propaguj(node);
-    int mid = (left + right) / 2;
-    chTree(2 * node, left, mid, l, r, v);
-    chTree(2 * node + 1, mid + 1, right, l, r, v);
-    update(node);
+
+    propagate(node);
+    int mid = lo + hi >> 1;
+    chTree(2 * node, lo, mid, l, r, v);
+    chTree(2 * node + 1, mid + 1, hi, l, r, v);
+    Tree[node] = Tree[2 * node] + Tree[2 * node + 1];
 }
 
-ll read(int node, int left, int right, int l, int r)
+ll read(int node, int lo, int hi, int l, int r)
 {
-    if (right < l || r < left) return 0;
-    if (l <= left && right <= r)
+    if (r < lo or hi < l) return 0;
+    if (l <= lo and hi <= r)
     {
         return Tree[node].sum;
     }
-    propaguj(node);
-    int mid = (left + right) / 2;
-    return read(2 * node, left, mid, l, r) +
-        +  read(2 * node + 1, mid + 1, right, l, r);
+
+    propagate(node);
+    int mid = lo + hi >> 1;
+    return read(2 * node, lo, mid, l, r) + read(2 * node + 1, mid + 1, hi, l, r);
 }
