683eba4794787f123ba591101b63130186f12d6d
jonparty <34322460+jonparty@users.noreply.github.com>
2018-09-14 21:53:26 +0200

    New implementation

--- Graphs/Shortest Paths/Bellman - Ford.cpp
+++ Graphs/Shortest Paths/Bellman - Ford.cpp
@@ -1,7 +1,6 @@
 // Computes shortest paths from the source to every other vertex
-// Returns 0 in case of a negative cycle
+// Returns 0 in case of a negative cycle reachable from the source
 // Complexity: O(|V| * |E|)
-// Usage: [SPOJ] NEGCYC http://www.spoj.com/problems/NEGCYC/
 
 #define st first
 #define nd second
@@ -9,6 +8,7 @@
 const int MAXN = 1e4 + 5, inf = 1e9 + 5;
 
 int dist[MAXN]; // Distance from the source
+bool vis[MAXN]; // An arry that marks which vertices are reachable from the source
 vector<pair<int, int>> G[MAXN];
 
 bool bellman_ford(int src, int n)
@@ -18,7 +18,8 @@ bool bellman_ford(int src, int n)
         dist[i] = inf;
     }
     dist[src] = 0;
-    
+
+    vis[src] = 1;
     for (int k = 1; k <= n; k++)
     {
         for (int v = 1; v <= n; v++)
@@ -28,12 +29,13 @@ bool bellman_ford(int src, int n)
                 int u = e.st, w = e.nd;
                 if (dist[u] > dist[v] + w)
                 {
+                    vis[u] |= vis[v];
                     dist[u] = dist[v] + w;
-                    if (k == n) return 0;
+                    if (k == n and vis[v]) return 0;
                 }
             }
         }
     }
-    
+
     return 1;
 }
