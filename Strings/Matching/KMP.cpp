0a691a99a398e44d48e1fbe0af2d92af588470eb
jonparty <34322460+jonparty@users.noreply.github.com>
2019-02-21 18:54:58 +0100

    Update KMP.cpp

--- Strings/Matching/KMP.cpp
+++ Strings/Matching/KMP.cpp
@@ -1,64 +1,22 @@
-// matchPattern returns list of occurrences of pattern p in string s
-// You can also match a pattern by creating KMP for string: p + 'S' + s
-// Complexity: O(|s| + |p|)
-// Source: wazniak.mimuw.edu.pl
+// Complexity: O(|s|)
 // Usage: [CF Round #246] Prefixes and Suffixes http://codeforces.com/problemset/problem/432/D
 
-struct KMP
-{
-    int N;
-    string p;
-    vector<int> ps;
+vector<int> KMP(string s) {
+    int n = s.size();
+    vector<int> ps(n);
 
-    KMP(string p) : N((int)p.size()), p(p), ps(N, 0)
-    {
-        int k = -1;
-        for (int i = 0; i < N; i++)
-        {
-            while (p[k] != p[i] && k >= 0)
-            {
-                if (k == 0) k = -1;
-                else k = ps[k - 1];
-            }
-            k++;
-            ps[i] = k;
-        }
-    }
-
-    vector<int> matchPattern(string s)
-    {
-        vector<int> occur;
-        int k = 0;
-        for (int i = 0; i < s.size();  i++)
-        {
-            while (p[k] != s[i] && k >= 0)
-            {
-                if (k == 0) k = -1;
-                else k = ps[k - 1];
-            }
-            k++;
-            if (k == N) occur.push_back(i - N + 1);
-        }
-        return occur;
-    }
-
-    int minCover()
-    {
-        vector<int> Range, Size;
-        for (int i = 0; i < N; i++)
-        {
-            Range.push_back(i);
-            Size.push_back(i + 1);
-        }
-
-        for (int i = 0; i < N; i++)
-        {
-            int sz = Size[P[i]-1];
-            if (P[i] && i - sz <= Range[sz - 1])
-            {
-                Size[i] = sz;
-                Range[sz - 1] = i;
+    int len = -1;
+    for (int i = 0; i < n; i++) {
+        while (len >= 0 && s[len] != s[i]) {
+            if (len == 0) {
+                len = -1;
+            } else {
+                len = ps[len - 1];
             }
         }
+
+        ps[i] = ++len;
     }
-};
+
+    return ps;
+}
