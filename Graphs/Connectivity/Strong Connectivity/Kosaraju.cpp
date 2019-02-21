6062382c972b017b14ef8583f5a0045f5b76f957
jonparty <34322460+jonparty@users.noreply.github.com>
2019-02-21 18:47:38 +0100

    Update Kosaraju.cpp

--- Graphs/Connectivity/Strong Connectivity/Kosaraju.cpp
+++ Graphs/Connectivity/Strong Connectivity/Kosaraju.cpp
@@ -3,65 +3,60 @@
 // 2) Processes vertices of the original graph in reverse postorder from 1)
 // Complexity: O(|V| + |E|)
 // Usage: [III POI] Agents https://szkopul.edu.pl/problemset/problem/lLzgarYSJUxLS5UXJHeyHl7U/site/?key=statement
-// Usage: [XIII POI] Professor Szu https://szkopul.edu.pl/problemset/problem/EijIYL4qkxxGJGHaVVnvdmgY/site/?key=statement
+//        [XIII POI] Professor Szu https://szkopul.edu.pl/problemset/problem/EijIYL4qkxxGJGHaVVnvdmgY/site/?key=statement
 
-const int MAXN = 1e4+5;
+const int MAXN = 1e6+5;
 
-int SCC[MAXN], nr; //SCC[v] - id of the SCC in which vertex v is
+int SCC[MAXN], nr; //SCC[v] - id of the SCC containing vertex v
 bool vis[MAXN]; // Visited vertices
-//Graf, graf transponowany, graf sinie spójnych
-// Graph, transpose graph, scc graph
+// Graph, transpose graph, graph of SCC:
 vector<int> G[MAXN], Gt[MAXN], Gscc[MAXN];
-stack<int> S; // Stack containing reverse postorder
+stack<int> St; // Stack containing reverse postorder
 
-void dfs(int v)
-{
+void dfs(int v) {
     vis[v] = 1;
-    for (int u : Gt[v])
-    {
-        if (!vis[u]) dfs(u);
+    for (int u : Gt[v]) {
+        if (!vis[u]) {
+            dfs(u);
+        }
     }
-    S.push(v);
+    St.push(v);
 }
 
-void findSCC(int v)
-{
-    vis[v] = 0, SCC[v] = nr;
-    for (int u : G[v])
-    {
-        if (!vis[u])
-        {
-            if (SCC[v] != SCC[u])
-            {
+void findSCC(int v) {
+    vis[v] = 1, SCC[v] = nr;
+    for (int u : G[v]) {
+        if (vis[u]) {
+            if (SCC[v] != SCC[u]) {
                 Gscc[SCC[v]].push_back(SCC[u]);
             }
             continue;
         }
-        calcSCC(u);
+        findSCC(u);
     }
 }
 
-void addEdge(int a, int b)
-{
-    // Adds edge to the graph
+void addEdge(int a, int b) {
+    // Adds edge (a, b) to the graph
     G[a].push_back(b);
-    // Adds edge to the transpose graph
+    // Adds edge (b, a) to the transpose graph
     Gt[b].push_back(a);
 }
 
-void sccBuild(n)
-{
-    for (int i = 1; i <= n; i++)
-    {
-        if (!vis[i]) dfs(i);
+void sccBuild(int n) {
+    for (int i = 1; i <= n; i++) {
+        if (!vis[i]) {
+            dfs(i);
+        }
     }
-    while (!S.empty())
-    {
-        int v = S.top();
-        S.pop();
-        if (vis[v])
-        {
-            calcSCC(v);
+
+    fill(vis, vis + n + 1, 0);
+    while (!St.empty()) {
+        int v = St.top();
+        St.pop();
+
+        if (!vis[v]) {
+            findSCC(v);
             nr++;
         }
     }
