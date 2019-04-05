de4cff2855973445da6241864c633d82ecfb8972
jonparty <34322460+jonparty@users.noreply.github.com>
2019-04-05 20:49:00 +0200

    Update Suffix Array using KMR.cpp

--- Strings/Suffixes/Suffix Array using KMR.cpp
+++ Strings/Suffixes/Suffix Array using KMR.cpp
@@ -1,135 +1,71 @@
-// Computing suffix array, LCP array and number of distinct substrings of string s
-// Suffix array is constructed using KMR with bucket sort
-// LCP is computed using Kasai's algorithm
+// Computing suffix array, LCP array and number of distinct substrings of string s.
+// Suffix array is constructed using KMR.
+// LCP is computed using Kasai's algorithm.
 // Complexity:
-// Suffix array: O(|s| * log |s|)
-// LCP: O(|s|)
-// Number of distinct substrings: O(|s|)
+// - Suffix array: O(|s| * log^2 |s|)
+// - LCP: O(|s|)
+// - Number of distinct substrings: O(|s|)
+// Source: Warsaw Eagles
 // Usage: [SPOJ] SUBST1 http://www.spoj.com/problems/SUBST1/
 
 using ll = long long;
 
-const int MAXN = 5e5 + 5;
+#define st first
+#define nd second
 
-struct Pair
-{
-    int h1, h2, pos;
-    bool operator != (const Pair &b) const
-    {
-        return (h1 != b.h1 || h2 != b.h2);
+struct SA {
+    vector<pair<pair<int, int>, int>> x;
+    vector<int> sa;
+    vector<int> rank;
+    vector<int> lcp;
+    vector<int> s;
+    int n;
+
+    SA(vector<int> s_) : s(s_) {
+        n = s.size();
+        x.resize(n);
+        rank = s;
+        for (int p = 1; p < n; p *= 2) {
+            for (int i = 0; i < n; i++) {
+                x[i] = {{rank[i], -1}, i};
+                if (i + p < n) {
+                    x[i].st.nd = rank[i + p];
+                }
+            }
+
+            sort(x.begin(), x.end());
+            int id = 0;
+            for (int i = 0; i < n; i++) {
+                if (i && x[i - 1].st != x[i].st) {
+                    id++;
+                }
+                rank[x[i].nd] = id;
+            }
+        }
+
+        sa.resize(n);
+        for (int i = 0; i < n; i++) {
+            sa[rank[i]] = i;
+        }
+
+        lcp.resize(n);
+        int len = 0;
+        for (int i = 0; i < n; i++) {
+            len = max(0, len - 1);
+            if (rank[i]) {
+                while (s[sa[rank[i]] + len] == s[sa[rank[i] - 1] + len]) {
+                    len++;
+                }
+            }
+            lcp[rank[i]] = len;
+        }
+    }
+
+    ll countDistinctSubstr() {
+        ll res = 0;
+        for (int i = 0; i < n; i++) {
+            res += n - sa[i] - lcp[i];
+        }
+        return res;
     }
 };
-
-// lex[i] - hash of the substring beginning in s[i]
-int lex[MAXN];
-vector<Pair> bucket[2][MAXN]; // Array for bucket sort
-
-void bucket_sort(vector<Pair> &pairs, int ALPHA)
-{
-    // Sorts by the second element of pair
-    for (auto p : pairs)
-    {
-        bucket[0][p.h2].push_back(p);
-    }
-
-    // Sorts by the first element of pair
-    for (int i = 0; i <= ALPHA; i++)
-    {
-        for (auto p : bucket[0][i])
-        {
-            bucket[1][p.h1].push_back(p);
-        }
-        bucket[0][i].clear();
-    }
-
-    // Fills the vector with sorted pairs
-    int ind = 0;
-    for (int i = 0; i <= ALPHA; i++)
-    {
-        for (auto p : bucket[1][i])
-        {
-            pairs[ind++] = p;
-        }
-        bucket[1][i].clear();
-    }
-}
-
-vector<int> saBuild(string &s)
-{
-    int n = s.size();
-    int ALPHA = 1; // Value of the largest hash
-    vector<Pair> pairs(n);
-
-    // Hashes of substring of length 1 are their ASCII numbers + 1
-    for (int i = 0; i < n; i++)
-    {
-        lex[i] = ((int)s[i] + 1);
-        ALPHA = max(ALPHA, lex[i]);
-    }
-
-    // Computes hashes for substrings of length 2 ^ (p + 1)
-    for (int p = 0; (1 << p) < n; p++)
-    {
-        for (int i = 0; i < n; i++)
-        {
-            Pair P = {lex[i], 0, i};
-            if (i + (1 << p) < n)
-            {
-                P.h2 = lex[i + (1 << p)];
-            }
-            pairs[i] = P;
-        }
-
-        // Bucket sort of all pairs
-        bucket_sort(pairs, ALPHA);
-
-        ALPHA = 1;
-
-        // Compresses hashes
-        lex[pairs.front().pos] = 1;
-        for (int i = 1; i < n; i++)
-        {
-            // If two substrings are the same they have the same hash
-            if (pairs[i] != pairs[i - 1]) ALPHA++;
-            lex[pairs[i].pos] = ALPHA;
-        }
-    }
-
-    // Fills the suffix array
-    vector<int> SA(n);
-    for (int i = 0; i < n; i++)
-    {
-        SA[lex[i] - 1] = i;
-    }
-    return SA;
-}
-
-vector<int> lcpBuild(string &s, vector<int> &SA)
-{
-    int n = s.size();
-    vector<int> LCP(n, 0);
-
-    int len = 0;
-    for (int i = 0; i < n; i++)
-    {
-        //LCP[i + 1] >= LCP[i] - 1
-        len = max(0, len - 1);
-        if (lex[i] == n) continue;
-        // Compares suffixes letter by letter
-        while (s[i + len] == s[ SA[lex[i]] + len])
-        {
-            len++;
-        }
-        LCP[lex[i] - 1] = len;
-    }
-    return LCP;
-}
-
-ll countDistinctSubstr(vector<int> &SA, vector<int> &LCP)
-{
-    int n = SA.size();
-    ll res = (ll)n * n;
-    for (int i : SA) res -= i;
-    for (int i : LCP) res -= i;
-    return res;
-}
