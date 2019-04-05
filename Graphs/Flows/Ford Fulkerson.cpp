db1489cca68e54212fb2b1ff0174c7a5235d7e22
jonparty <34322460+jonparty@users.noreply.github.com>
2019-04-06 00:06:34 +0200

    Update Ford Fulkerson.cpp

--- Graphs/Flows/Ford Fulkerson.cpp
+++ Graphs/Flows/Ford Fulkerson.cpp
@@ -3,54 +3,54 @@
 // Usage: [SPOJ] FLOW http://www.spoj.com/A12/problems/FLOW/
 //        [VI POI] Speleology https://szkopul.edu.pl/problemset/problem/l7RiYCdarLZV51E2daTt4n4W/site/?key=statement
 
+using ll = long long;
+
 #define st first
 #define nd second
 
-const int MAXN = 205, INF = 1e9;
+const int MAXN = 5e3 + 5, inf = 1e9;
 
-// Capacities, visited vertices, id of the target
-int E[MAXN*MAXN], vis[MAXN], t, id, tim = 1;
-vector<pair<int, int>> G[MAXN]; // Graph
+int E[MAXN * MAXN], vis[MAXN], n, tgt, id, tim = 1;
+vector<pair<int, int>> G[MAXN];
 
-// Adds edge (a, b) with capacity c and its opposite edge
-void addEdge(int a, int b, int c)
-{
+inline void addEdge(int a, int b, int c) {
     G[a].push_back({b, id});
     E[id++] = c;
     G[b].push_back({a, id++});
 }
 
-int dfs(int v, int flow)
-{
-    if (v == t)
-    {
-        // You've reached the target = found an augmenting path
+int dfs(int v, int flow) {
+    if (v == tgt) {
         return flow;
     }
     vis[v] = tim;
-    for (auto e : G[v])
-    {
+
+    for (auto e : G[v]) {
         int u = e.st, x = e.nd;
-        if (vis[u] == tim) continue;
-        int w = E[x], f = 0;
-        if (w) f = dfs(u, min(flow, w));
-        if (f)
-        {
-            E[x] -= f;
-            E[x^1] += f;
-            return f;
+        if (vis[u] == tim) {
+            continue;
         }
+
+        int f = E[x];
+        if (f) {
+            f = dfs(u, min(flow, f));
+        }
+        if (not f) {
+            continue;
+        }
+
+        E[x] -= f;
+        E[x ^ 1] += f;
+        return f;
     }
-    return 0; // There aren't any augmenting paths in the graph
+    return 0;
 }
 
-long long maxflow(int src)
-{
-    long long flow = 0;
-    while (int f = dfs(src, INF))
-    {
-        flow += f;
+ll maxflow(int src) {
+    ll flow = 0;
+    while (int f = dfs(src, inf)) {
         tim++;
+        flow += f;
     }
     return flow;
 }
