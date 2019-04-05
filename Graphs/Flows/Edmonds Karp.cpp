9c62cbbd5752f9a3de179d0be7b2d47103cd5e92
jonparty <34322460+jonparty@users.noreply.github.com>
2019-04-06 00:20:45 +0200

    Update Edmonds Karp.cpp

--- Graphs/Flows/Edmonds Karp.cpp
+++ Graphs/Flows/Edmonds Karp.cpp
@@ -1,74 +1,69 @@
 // Computes maxflow using Edmonds - Karp algorithm
-// Complexity: O(|V| * |E|^2)
+// Complexity: O(V * E^2)
 // Usage: [SPOJ] FLOW http://www.spoj.com/A12/problems/FLOW/
 //        [VI POI] Speleology https://szkopul.edu.pl/problemset/problem/l7RiYCdarLZV51E2daTt4n4W/site/?key=statement
 //        [XVIII POI] Programming Contest https://szkopul.edu.pl/problemset/problem/VwDLJhYqi1z_sZrb2NyfvQ5e/site/?key=statement
 //        [XVII POI] Bridges https://szkopul.edu.pl/problemset/problem/gh2Yj6Ckrt4Lo_RojONuljuC/site/?key=statement
 
+using ll = long long;
+
 #define st first
 #define nd second
 
-const int MAXN = 205, INF = 1e9;
+const int MAXN = 5e3 + 5, inf = 1e9;
 
-// Capacities, visited vertices, id of the target
-int E[MAXN*MAXN], vis[MAXN], t, id, tim;
-pair<int, int> p[MAXN]; // Predecessors in BFS
-vector<pair<int, int>> G[MAXN]; // Graph
+int E[MAXN * MAXN], vis[MAXN], tgt, id, tim = 1;
+pair<int, int> p[MAXN];
+vector<pair<int, int>> G[MAXN];
 
-// Adds edge (a, b) with capacity c and its opposite edge
-void addEdge(int a, int b, int c)
-{
+inline void addEdge(int a, int b, int c) {
     G[a].push_back({b, id});
     E[id++] = c;
     G[b].push_back({a, id++});
 }
 
-int bfs(int src)
-{
+bool bfs(int src) {
     queue<pair<int, int>> Q;
-    vis[src] = ++tim;
-    Q.push({src, INF});
-    while (!Q.empty())
-    {
-        int v = Q.front().st, flow = Q.front().nd;
+    Q.push({src, inf});
+    vis[src] = 0;
+    while (not Q.empty()) {
+        int v = Q.front().st, f = Q.front().nd;
         Q.pop();
-        for (auto e : G[v])
-        {
+        if (not f) {
+            continue;
+        }
+
+        for (auto e : G[v]) {
             int u = e.st, x = e.nd;
-            if (vis[u] == tim) continue;
-            int w = E[x];
-            if (w > 0)
-            {
-                flow = min(flow, w);
-                vis[u] = tim;
-                p[u] = {v, x};
-                if (u == t)
-                {
-                    // You've reached the target = found an augmenting path
-                    return flow;
-                }
-                Q.push({u, flow});
+            if (not E[x] or vis[u] == tim) {
+                continue;
             }
+
+            p[u] = {v, x};
+            if (u == tgt) {
+                return f;
+            }
+            vis[v] = tim;
+            Q.push({u, min(f, E[x])});
+
         }
     }
-    return 0; // There aren't any augmenting paths in the graph
+
+    return 0;
 }
 
-long long maxflow(int src)
-{
-    long long flow = 0;
-    while (int f = bfs(src))
-    {
-        flow += f;
-        // Updates capacities
-        int cur = t;
-        while (cur)
-        {
-            int x = p[cur].nd;
+ll maxflow(int src) {
+    ll flow = 0;
+    while (int f = bfs(src)) {
+        int u = tgt;
+        while (u != src) {
+            int v = p[u].st, x = p[u].nd;
             E[x] -= f;
-            E[x^1] += f;
-            cur = p[cur].st;
+            E[x ^ 1] += f;
+            u = v;
         }
+        tim++;
+        flow += f;
     }
     return flow;
 }
