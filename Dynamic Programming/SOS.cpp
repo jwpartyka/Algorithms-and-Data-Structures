7ee4ca138bf90bd5b1895a3ec70c24439c648f3b
Janusz Partyka <janusz.partyka00@gmail.com>
2019-07-18 23:10:40 +0200

    Added SOS

---
+++ Dynamic Programming/SOS.cpp
@@ -0,0 +1,12 @@
+vector<int> SOS(vector<int> &a) {
+  int n = 31 - __builtin_clz(a.size());
+  auto dp = a;
+  for (int i = 0; i < n; ++i) {
+    for (int mask = 0; mask < (1 << n); ++mask) {
+      if (mask & (1 << i)) {
+        dp[mask] += dp[mask ^ (1 << i)];
+      }
+    }
+  }
+  return dp;
+}
